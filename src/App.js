e9c5e143c89f9fcaee172ede70f5045bf7202aed
Dieferson Silveira <dieferson.zoka@gmail.com>
2025-05-29 16:38:40 -0300

    chore: ajustes na estrutura base do projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import {
 } from "react-router-dom";
 
 import PaginaInicial from "./pages/PaginaInicial/PaginaInicial";
-import PainelAdmin from "./pages/Admin/PainelAdmin";
+
+import DashboardAdmin from "./pages/Admin/DashboardAdmin/DashboardAdmin";
+import ListaPacientes from "./pages/Admin/ListaPacientes/ListaPacientes";
+import Agenda from "./pages/Admin/Agenda/Agenda";
+import Graficos from "./pages/Admin/Graficos/Graficos";
+import Psicologos from "./pages/Admin/Psicologos/Psicologos";
+import Consultas from "./pages/Admin/Consultas/Consultas";
 
 function App() {
   return (
@@ -16,10 +22,18 @@ function App() {
         <div className="conteudo">
           <Routes>
             <Route path="/" element={<PaginaInicial />} />
-            <Route path="/admin" element={<PainelAdmin />} />
+
+            <Route path="/admin" element={<DashboardAdmin />} />
+            <Route path="/admin/pacientes" element={<ListaPacientes />} />
+            <Route path="/admin/agenda" element={<Agenda />} />
+            <Route path="/admin/graficos" element={<Graficos />} />
+            <Route path="/admin/psicologos" element={<Psicologos />} />
+            <Route path="/admin/consultas" element={<Consultas />} />
+
             <Route path="/homepage" element={<Navigate to="/" replace />} />
             <Route path="/login" element={<Navigate to="/" replace />} />
             <Route path="/cadastro" element={<Navigate to="/" replace />} />
+
             <Route path="*" element={<div>Página não encontrada</div>} />
           </Routes>
         </div>
